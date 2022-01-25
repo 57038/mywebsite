3db8192c73be0cc555511b65ad784832dc1571bf
57038 <97937760+57038@users.noreply.github.com>
2022-01-25 13:48:30 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -3,6 +3,6 @@
                 <title>งานที่ 1</title>
         </head>
         <body bgcolor="lightblue">
-                page1ธรรมดา - - - <b>page1 หนา</b>
+                <b>page1ธรรมดา - - - <b>page1 หนา</b>
         </body>
 </html>
