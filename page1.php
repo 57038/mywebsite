fcad204452940550bc4a56aa023d413da1e31c0d
57038 <97937760+57038@users.noreply.github.com>
2022-01-25 14:25:49 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,7 +1,18 @@
 <html>
-        <head>
-                <title>งานที่ 1</title>
-        </head>
-        <body bgcolor="lightblue">
+      <head>
+            <title>ประวัติส่วนตัว</title>
+      </head>
+      <body bgcolor="lightblue">
+                  <b>ชื่อ : </b> <i> น.ส.มลฑกาญจ์  บุญประสพ </i>
+                  <br>
+                  <b>ชื่อเล่น : </b> <i> สตางค์ </i>
+                  <br>
+                  <b>ชั้น : </b> <i> ม.4/13 </i>
+                  <br>
+                  <b>เลขที่ : </b> <i> 21 </i>
+                  <br>
+                  <b>เลขประจำตัวนักเรียน : </b> <i> 57038 </i>
+                  <br>
+                  <b>โรงเรียน : </b> <i> หล่มสักวิทยาคม </i>
         </body>
 </html>
