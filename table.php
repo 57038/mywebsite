4528b1fc059443e0cb40a914e7a1035a1fd35a7d
57038 <97937760+57038@users.noreply.github.com>
2022-02-08 15:07:56 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,4 +1,4 @@
-<html>
+ <html>
   <head>
     <title>ตารางของฉัน</title>
   </head>
@@ -6,14 +6,18 @@
     <table border="1">
       <tr>
         <th>ลำดับ</th>
-        <th>ชื่อ-นามสกุล</th>
+        <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>มลฑกาญจ์  บุญประสพ</td>
-        <td>สตางค์</td>
-      </tr>
+        <td>ปัญญ์ธิษา เธียรประสิทธิ์</td>
+        <td>หวาย</td>
+        <td><img src="https://s.isanook.com/me/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL21lLzAvdWQvMTAvNTEzMjEvNzc3LmpwZw==.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/wanderwaii?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+  </tr>  
     </table>
   </body> 
-</html>  
+</html>
