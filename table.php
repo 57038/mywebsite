f2cda056cea9b9d7b2993ca158e5916bbe87bebe
57038 <97937760+57038@users.noreply.github.com>
2022-02-08 15:57:01 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,7 +17,42 @@
         <td>หวาย</td>
         <td><img src="https://s.isanook.com/me/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL21lLzAvdWQvMTAvNTEzMjEvNTYyMDAwMDAzOTMxOTA5LmpwZw==.jpg" width="100px"></td>
         <td><a href="https://twitter.com/wanderwaii?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
-  </tr>  
+     </tr> 
+     <tr>
+        <td>2</td>
+        <td>พรนับพัน พรเพ็ญพิพัฒน์</td>
+        <td>เนเน่</td>
+        <td><img src="https://img.soccersuck.com/images/2020/06/04/42f85a4bcae8f6f58bb50c9464b970b8.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/nenezhengnaixin">twitter</a></td>
+     </tr>
+     <tr>
+        <td>3</td>
+        <td>ธนันต์ธรญ์ นีระสิงห์</td>
+        <td>ฟาง</td>
+        <td><img src="https://adaymagazine.com/wp-content/uploads/2021/03/Artboard-18-copy-8-100-682x1024.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/fangfanggn">twitter</a></td>
+     </tr>  
+     <tr>
+        <td>4</td>
+        <td>이지은</td>
+        <td>ไอยู</td>
+        <td><img src="https://nineentertain.mcot.net/wp-content/uploads/2021/05/bc2bd95db3dc462b0722ebf80745561c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/_iuofficial">twitter</a></td>
+     </tr>
+     <tr>
+        <td>5</td>
+        <td>김태형</td>
+        <td>วี</td>
+        <td><img src="https://us-fbcloud.net/quiz/data/21/21693.image1.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt">twitter</a></td>
+     </tr>  
+     <tr>
+        <td>6</td>
+        <td> 周震南</td>
+        <td>หนานน่าน</td>
+        <td><img src="https://pin.it/5IHUslP" width="100px"></td>
+        <td><a href="https://twitter.com/R1SE_TH?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+     </tr>  
     </table>
   </body> 
 </html>
