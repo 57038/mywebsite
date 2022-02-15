4eac543b6e90861c76f6cc37903f7040d8b09fb0
57038 <97937760+57038@users.noreply.github.com>
2022-02-15 14:52:13 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,8 +1,25 @@
 <html>
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color:#AFD7F6;
+			}
+			body{
+				background-color:#FEB9D0;
+			}
+			h1{
+				color:#F9EEB6;
+			}
+		</style>	
 	</head>
 	<body>
-    <p style="color:green;">ข้อความนี้สีเขียว</p>
+		<div id="box">
+			<h1>นางสาวมลฑกาญจ์ บุญประสพ เลขที่ 21 ชั้น ม.4/13</h1>
+		</div>
 	</body>
 </html>
