f63a5397c434b4280b06a492af4e74f8e179de4e
57038 <97937760+57038@users.noreply.github.com>
2022-02-22 14:20:58 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(ChocolateteehaypaiTEST.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(baozi.woff)
+    }
+    #font1{
+	font-family: myFont1;    
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวมลฑกาญจ์ บุญประสพ ม.4/13 เลขที่ 21 เลขประจำตัว 57038</h1>
+  <h1 id="font2">นางสาวมลฑกาญจ์ บุญประสพ ม.4/13 เลขที่ 21 เลขประจำตัว 57038</h1>
+</body>
+</html>
